7e0018effa2ffbf9535a731f17fc5cf54fadebc8
TwuitePanda <glauco.laurent@gmail.com>
2024-07-16 14:47:46 -0300

    atualizado

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,42 +1,38 @@
+import React from 'react'
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../service/api' // Ajuste o caminho se necessário
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
+interface Props {
   favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
   favoritar: (produto: ProdutoType) => void
 }
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+const ProdutosComponent: React.FC<Props> = ({ favoritos, favoritar }) => {
+  const { data: produtos, isLoading, error } = useGetProdutosQuery()
+
+  if (isLoading) return <h2>Carregando...</h2>
+  if (error) return <h2>Erro ao carregar os dados</h2>
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
     const IdsDosFavoritos = favoritos.map((f) => f.id)
-
     return IdsDosFavoritos.includes(produtoId)
   }
 
   return (
-    <>
-      <S.Produtos>
-        {produtos.map((produto) => (
-          <Produto
-            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
-            key={produto.id}
-            produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
-          />
-        ))}
-      </S.Produtos>
-    </>
+    <S.Produtos>
+      {produtos?.map((produto) => (
+        <Produto
+          estaNosFavoritos={produtoEstaNosFavoritos(produto)}
+          key={produto.id}
+          produto={produto}
+          favoritar={favoritar}
+        />
+      ))}
+    </S.Produtos>
   )
 }
 
